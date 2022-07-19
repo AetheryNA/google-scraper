3743871de835580cedd3150f112441b10d560780
AetheryNA <mrnaaw98@gmail.com>
2022-07-20 01:33:59 +0530

    [CH003] Big UI improvements for the dashboard and sign in and sign up forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,19 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      blue: '#1fb6ff',
+      black: {
+        default: '#353b48',
+      },
+      white: {
+        default: '#fff',
+        500: '#f5f6fa',
+        600: '#dcdde1',
+      },
+      blue: {
+        default: '#0097e6',
+      },
       purple: '#a855f7',
       pink: '#ec4899',
-      orange: '#ff7849',
-      green: '#13ce66',
-      yellow: '#ffc82c',
-      gray: '#8492a6',
-      white: '#fff',
       indigo: '#6366f1',
     },
   },
