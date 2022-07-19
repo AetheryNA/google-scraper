25d8d38d37e045d92e2ce34a91a0ff40b4f19100
AetheryNA <mrnaaw98@gmail.com>
2022-07-19 15:33:12 +0530

    [F003] Adjusted Cypress tests and added test helper functions

---
+++ cypress/test.helper.ts
@@ -0,0 +1,17 @@
+export const registerNewUser = (createUser: any) => {
+  cy.visit('/users/sign-up');
+
+  cy.get('.auth-form__username').type(createUser.username);
+  cy.get('.auth-form__password').type(createUser.password);
+
+  cy.get('.auth-form__button').click();
+};
+
+export const loginUser = (user: any) => {
+  cy.visit('/users/sign-in');
+
+  cy.get('.auth-form__username').type(user.username);
+  cy.get('.auth-form__password').type(user.password);
+
+  cy.get('.auth-form__button').click();
+};
