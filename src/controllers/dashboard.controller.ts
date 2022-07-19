72c30f2b9aac0cc791373c803e11b38a1fce5ad3
AetheryNA <mrnaaw98@gmail.com>
2022-07-19 15:34:18 +0530

    [F003] Created dashboard controller for uploading files, limited to CSV's only

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get, Render, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Render,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+  ParseFilePipe,
+  FileTypeValidator,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { readFileSync } from 'fs';
 import { AuthenticatedGuard } from 'src/common/guards/authenticated.guard';
 import { UserService } from 'src/services/user.service';
+import { diskStorage } from 'multer';
 
 @Controller('dashboard')
 export class DashboardController {
@@ -11,5 +24,19 @@ export class DashboardController {
   @Render('dashboard')
   async renderDashboard() {
     return null;
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Post('/upload-file')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'csv' })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return file;
   }
 }
