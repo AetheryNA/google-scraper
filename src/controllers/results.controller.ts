23f20739d9aa5cac4071810d205a20ab05e21026
AetheryNA <mrnaaw98@gmail.com>
2022-07-22 09:27:10 +0530

    [F007] Created controller and service for search bar query

--- src/controllers/results.controller.ts
+++ src/controllers/results.controller.ts
@@ -1,10 +1,27 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Query, Req } from '@nestjs/common';
+import { ResultsService } from 'src/services/results.service';
+import { Request } from 'express';
 
 @Controller('results')
 export class ResultsController {
+  constructor(private resultsService: ResultsService) {}
+
   @Get()
   @Render('results')
   async renderResultsPage() {
     return null;
+  }
+
+  @Get('/search-keywords')
+  @Render('results')
+  async searchKeyword(@Query() queryParam: any, @Req() req: Request) {
+    const queryValue = await this.resultsService.findKeywordsByUserId(
+      1,
+      queryParam.keywordToSearch,
+    );
+
+    return {
+      queriedValue: queryValue,
+    };
   }
 }
--- src/modules/results.module.ts
+++ src/modules/results.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResultsController } from 'src/controllers/results.controller';
+import { Keywords } from 'src/entities/keywords.entity';
+import { ResultsService } from 'src/services/results.service';
+import { UserModule } from './user.module';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [UserModule, TypeOrmModule.forFeature([Keywords])],
+  providers: [ResultsService],
   controllers: [ResultsController],
 })
 export class ResultsModule {}
